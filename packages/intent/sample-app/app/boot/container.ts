dc50d93fa5a1f5930697643326bcf2cdeaa50574
Alexander Cerutti <cerutti.alexander@gmail.com>
2025-01-05 23:50:38 +0100

    Removed greetings example

--- packages/intent/sample-app/app/boot/container.ts
+++ packages/intent/sample-app/app/boot/container.ts
@@ -1,6 +1,5 @@
 import { IntentAppContainer, IntentProvidersFactory } from "@intentjs/core";
 import { AppServiceProvider } from "./sp/app.js";
-import { ConsoleServiceProvider } from "./sp/console.js";
 import config from "../../config/index.js";
 
 export class ApplicationContainer extends IntentAppContainer {
@@ -16,10 +15,5 @@ export class ApplicationContainer extends IntentAppContainer {
 		 * Register our main application service providers.
 		 */
 		this.add(AppServiceProvider);
-
-		/**
-		 * Registering our console commands service providers.
-		 */
-		this.add(ConsoleServiceProvider);
 	}
 }
