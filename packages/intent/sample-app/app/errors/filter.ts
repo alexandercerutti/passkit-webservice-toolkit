4b8cdfc208ea0dcfc491f04ec111ce8a18c7639c
Alexander Cerutti <cerutti.alexander@gmail.com>
2025-01-05 23:45:50 +0100

    Prettier format

--- packages/intent/sample-app/app/errors/filter.ts
+++ packages/intent/sample-app/app/errors/filter.ts
@@ -1,25 +1,25 @@
 import {
-  ConfigService,
-  ExecutionContext,
-  HttpException,
-  IntentExceptionFilter,
-  Type,
-} from '@intentjs/core';
+	ConfigService,
+	ExecutionContext,
+	HttpException,
+	IntentExceptionFilter,
+	Type,
+} from "@intentjs/core";
 
 export class ApplicationExceptionFilter extends IntentExceptionFilter {
-  constructor(private config: ConfigService) {
-    super();
-  }
+	constructor(private config: ConfigService) {
+		super();
+	}
 
-  doNotReport(): Array<Type<HttpException>> {
-    return [];
-  }
+	doNotReport(): Array<Type<HttpException>> {
+		return [];
+	}
 
-  report(): Array<Type<HttpException>> | string {
-    return '*';
-  }
+	report(): Array<Type<HttpException>> | string {
+		return "*";
+	}
 
-  handleHttp(context: ExecutionContext, exception: any) {
-    return super.handleHttp(context, exception);
-  }
+	handleHttp(context: ExecutionContext, exception: any) {
+		return super.handleHttp(context, exception);
+	}
 }
--- packages/intent/sample-app/config/localization.ts
+++ packages/intent/sample-app/config/localization.ts
@@ -1,28 +1,28 @@
-import { LocalizationOptions, configNamespace } from '@intentjs/core';
+import { LocalizationOptions, configNamespace } from "@intentjs/core";
 
 export default configNamespace(
-  'localization',
-  (): LocalizationOptions => ({
-    /**
-     * -----------------------------------------------------
-     * Locale Path
-     * -----------------------------------------------------
-     * Documentation - https://tryintent.com/docs/localization
-     *
-     * This value is the name of the default disk. This will be
-     * used when you use the `Storage` facade to access your
-     * files.
-     */
-    path: 'resources/lang',
+	"localization",
+	(): LocalizationOptions => ({
+		/**
+		 * -----------------------------------------------------
+		 * Locale Path
+		 * -----------------------------------------------------
+		 * Documentation - https://tryintent.com/docs/localization
+		 *
+		 * This value is the name of the default disk. This will be
+		 * used when you use the `Storage` facade to access your
+		 * files.
+		 */
+		path: "resources/lang",
 
-    /**
-     * -----------------------------------------------------
-     * Fallback Lang
-     * -----------------------------------------------------
-     *
-     * This value is used to define the default lang that
-     * will be used when you are using translations in your app.
-     */
-    fallbackLang: 'en',
-  }),
+		/**
+		 * -----------------------------------------------------
+		 * Fallback Lang
+		 * -----------------------------------------------------
+		 *
+		 * This value is used to define the default lang that
+		 * will be used when you are using translations in your app.
+		 */
+		fallbackLang: "en",
+	}),
 );
