a5c3bad97278b64da45a10584824caa691c5e486
Alexander Cerutti <cerutti.alexander@gmail.com>
2025-01-05 23:51:51 +0100

    Added testpass endpoint definition to create a pass

--- packages/intent/sample-app/app/http/controllers/index.ts
+++ packages/intent/sample-app/app/http/controllers/index.ts
@@ -1,14 +1,40 @@
 import { Controller, Get, Req, Request, Res, Response } from "@intentjs/core";
 import { IndexService } from "app/services/index.js";
+import { createPass } from "app/utils/index.js";
 
 @Controller()
 export class IndexController {
+	#lastPassUpdate = Date.now();
+
 	constructor(private readonly service: IndexService) {
 		console.log(service);
+	}
+
+	get passUpdateDate(): number {
+		return this.#lastPassUpdate;
+	}
+
+	set passUpdateDate(value: number) {
+		this.#lastPassUpdate = value;
 	}
 
 	@Get()
 	async getHello(@Req() req: Request, @Res() res: Response) {
 		return res.json({ hello: "world" });
+	}
+
+	@Get("/testpass")
+	async getInitialPass(@Req() req: Request, @Res() res: Response) {
+		const pass = await createPass();
+
+		this.#lastPassUpdate = Date.now();
+
+		res.setHeaders({
+			"Content-Type": pass.mimeType,
+			"Content-Disposition": `attachment; filename="pass.pkpass"`,
+			"Last-Modified": new Date().toUTCString(),
+		});
+
+		res.status(200).send(pass.getAsBuffer());
 	}
 }
---
+++ packages/intent/sample-app/app/utils/index.ts
@@ -0,0 +1,118 @@
+import { networkInterfaces } from "node:os";
+import fs from "node:fs";
+import path from "node:path";
+import passKit from "passkit-generator";
+const { PKPass } = passKit;
+
+const __dirname = import.meta.dirname;
+console.log("DIRNAME", __dirname);
+
+const intentPluginPackageRoot = path.resolve(__dirname, "../../../..");
+const workspaceRoot = path.resolve(intentPluginPackageRoot, "../..");
+const passkitGeneratorProjectRoot = path.resolve(
+	workspaceRoot,
+	"../passkit-generator",
+);
+
+const IPV4Interfaces: Record<string, { address: string }[]> = {};
+
+for (const [ifName, netIf] of Object.entries(networkInterfaces())) {
+	if (!netIf?.length) {
+		continue;
+	}
+
+	for (let i = 0; i < netIf.length; i++) {
+		const netInterface = netIf[i];
+		const isIPv4 = netInterface.family == "IPv4";
+		const isLoopback = netInterface.address === "127.0.0.1";
+
+		const isSuitable = isIPv4 && !isLoopback && !netInterface.internal;
+
+		if (isSuitable) {
+			IPV4Interfaces[ifName] = IPV4Interfaces[ifName] || [];
+			IPV4Interfaces[ifName].push(netInterface);
+		}
+	}
+}
+
+const AIRPORTS = {
+	VCE: "Venice Marco Polo Airport",
+	AMS: "Amsterdam Airport Schipol",
+	MUC: "Munich Airport",
+	FRA: "Frankfurt Airport",
+	LAX: "Los Angeles International Airport",
+	DXB: "Dubai International Airport",
+	IAD: "Dulles International Airport",
+	HND: "Haneda Airport",
+	LHR: "Heathrow Airport",
+	MEL: "Melbourne Airport",
+	ZRH: "Zurich Airport",
+} as const;
+
+function getRandomAirport(current: string = "") {
+	const list = Object.keys(AIRPORTS);
+
+	let selected = current;
+
+	while (selected === current || !selected) {
+		selected = list[Math.floor(Math.random() * (list.length - 0 + 1) + 0)];
+	}
+
+	return selected;
+}
+
+export async function createPass(
+	modifications?: Record<string, string>,
+	serialNumber = String(Math.random() * 100),
+) {
+	const pass = await PKPass.from(
+		{
+			model: `${passkitGeneratorProjectRoot}/examples/models/exampleBooking.pass`,
+			certificates: {
+				signerCert: fs.readFileSync(
+					`${passkitGeneratorProjectRoot}/certificates/nfc/signerCert.pem`,
+				),
+				signerKey: fs.readFileSync(
+					`${passkitGeneratorProjectRoot}/certificates/nfc/signerKey.pem`,
+				),
+				wwdr: fs.readFileSync(
+					`${passkitGeneratorProjectRoot}/certificates/WWDRG4.pem`,
+				),
+				signerKeyPassphrase: "123456",
+			},
+		},
+		{
+			serialNumber,
+			webServiceURL: `http://${
+				Object.entries(IPV4Interfaces)[0][1][0].address
+			}:3500`,
+			authenticationToken: "mimmomimmoqgeqwyidukqq",
+			voided: false,
+			...modifications,
+		},
+	);
+
+	pass.transitType = "PKTransitTypeAir";
+
+	const departureAirport = getRandomAirport();
+	const arrivalAirport = getRandomAirport(departureAirport);
+
+	pass.primaryFields.push({
+		key: "Departure",
+		label: AIRPORTS[departureAirport],
+		value: departureAirport,
+	});
+
+	pass.primaryFields.push({
+		key: "Destination",
+		label: AIRPORTS[arrivalAirport],
+		value: arrivalAirport,
+		changeMessage: "Destination airport changed to %@",
+	});
+
+	pass.setExpirationDate(null);
+	pass.setRelevantDate(null);
+	pass.setLocations(null);
+
+	return pass;
+}
